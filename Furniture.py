e0973fdb49c2ef93942a84518f7bbb1ea5922494
A5T0R <joshlpeck@live.com>
2021-07-10 00:08:36 +0100

    Furniture commit

---
+++ Furniture.py
@@ -0,0 +1,13 @@
+from DrawingTools import *
+
+class Table():
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def draw(self,anchor_x,anchor_y):
+        draw_box( anchor_x, anchor_y, 3, 1, 'C', Fore.BLUE, Back.GREEN, True )
+        draw_box( anchor_x, anchor_y +2, 3, 2, 'T', Fore.GREEN, Back.BLUE, True )
+        draw_box( anchor_x, anchor_y +5, 3, 1, 'C', Fore.BLUE, Back.GREEN, True )
+
