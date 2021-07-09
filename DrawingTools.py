02f72b43edac1563d1211850c01731a46aab9f8b
Rich Mayo <rich.mayo@live.co.uk>
2021-07-09 22:08:58 +0100

    added draw_box helper

--- DrawingTools.py
+++ DrawingTools.py
@@ -9,3 +9,18 @@ def pos(x,y):
 
 def clearscreen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def draw_box( x_anchor, y_anchor, width, height, letter, back_colour, fore_colour, solid ):
+        doubleletter = letter+letter
+        print(back_colour, end='')
+        print(fore_colour, end='')
+        print('%s%s' % (pos(x_anchor, y_anchor), letter*width*2), end='')
+        for y in range(1, height):
+            if solid:
+                print('%s%s' % (pos(x_anchor, y), letter*width*2), end='')
+            else:
+                print('%s%s%s%s' % (pos(1, y),letter + letter,pos((width*2)-1, y), letter + letter), end='')
+        print('%s%s' % (pos(1, height), letter*width*2), end='')
+        print(pos(1,height+2))
+        print(Back.BLACK, end='')
+        
--- Tavern.py
+++ Tavern.py
@@ -9,15 +9,9 @@ class Tavern:
         self.name = n
 
     def draw(self):
-        print(Back.WHITE, end='')
-        print(Fore.RED, end='')
-        print('%s%s' % (pos(1, 1), 'W'*self.w*2), end='')
-        for y in range(1, self.h):
-            print('%s%s%s%s' % (pos(1, y),'WW',pos((self.w*2)-1, y), 'WW'), end='')
-        print('%s%s' % (pos(1, self.h), 'W'*self.w*2), end='')
-        print(pos(1,self.h+2))
-        print(Back.BLACK, end='')
-        print(f'{self.name}')
+
+        draw_box( 1, 1, 16, 16, 'W', Fore.RED, Back.WHITE, True )
+        print(self.name)
 
 
         
